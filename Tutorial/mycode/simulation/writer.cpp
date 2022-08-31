0a15cf5f20933f737229742fc0dbeb9f029995cf
calgray <callan.gray@icrar.org>
2022-08-31 13:19:48 +0800

    2 writers

--- Tutorial/mycode/simulation/writer.cpp
+++ Tutorial/mycode/simulation/writer.cpp
@@ -39,15 +39,6 @@ Writer::Writer(const Settings &settings, const GrayScott &sim,
 
     // information: <int> step   is a single value
 
-    varStep = _io.DefineVariable<int>("step");
-    varU =
-        _io.DefineVariable<double>("U", {settings.L, settings.L, settings.L},
-                                   {sim.offset_z, sim.offset_y, sim.offset_x},
-                                   {sim.size_z, sim.size_y, sim.size_x}, true);
-    varU.SetMemorySelection(
-        {{1, 1, 1}, {sim.size_z + 2, sim.size_y + 2, sim.size_x + 2}});
-    _io.DefineAttribute<double>("F", settings.F);
-
     /*
      *  MPI Subarray data type for writing/reading parallel distributed arrays
      *  See case II for array with ghost cells:
@@ -76,43 +67,37 @@ Writer::Writer(const Settings &settings, const GrayScott &sim,
 
 void Writer::open(const std::string &fname)
 {
-    _writer = _io.Open(fname, adios2::Mode::Write);
+    int cmode;
+    MPI_Info info;
+    MPI_Offset headersize = sizeof(header);
+
+    /* Users can set customized I/O hints in info object */
+    info = MPI_INFO_NULL; /* no user I/O hint */
+
+    /* set file open mode */
+    cmode = MPI_MODE_CREATE;  /* to create a new file */
+    cmode |= MPI_MODE_WRONLY; /* with write-only permission */
+
+    /* collectively open a file, shared by all processes in MPI_COMM_WORLD */
+    std::string s = fname + ".u";
+    err = MPI_File_open(comm, s.c_str(), cmode, info, &fh);
+    CHECK_ERR(MPI_File_open to write)
+
+    int rank;
+    MPI_Comm_rank(comm, &rank);
+    if (!rank)
+    {
+        unsigned long long L = static_cast<unsigned long long>(settings.L);
+        struct header h = {L, L, L};
+        MPI_Status status;
+        MPI_File_write(fh, &h, sizeof(h), MPI_BYTE, &status);
+    }
+
+    err =
+        MPI_File_set_view(fh, headersize, MPI_DOUBLE, filetype, "native", info);
+    CHECK_ERR(MPI_File_set_view)
+    MPI_Barrier(comm);
 }
-
-// void Writer::open(const std::string &fname)
-// {
-//     int cmode;
-//     MPI_Info info;
-//     MPI_Offset headersize = sizeof(header);
-
-//     /* Users can set customized I/O hints in info object */
-//     info = MPI_INFO_NULL; /* no user I/O hint */
-
-//     /* set file open mode */
-//     cmode = MPI_MODE_CREATE;  /* to create a new file */
-//     cmode |= MPI_MODE_WRONLY; /* with write-only permission */
-
-//     /* collectively open a file, shared by all processes in MPI_COMM_WORLD */
-//     std::string s = fname + ".u";
-//     err = MPI_File_open(comm, s.c_str(), cmode, info, &fh);
-//     CHECK_ERR(MPI_File_open to write)
-
-//     int rank;
-//     MPI_Comm_rank(comm, &rank);
-//     if (!rank)
-//     {
-//         unsigned long long L = static_cast<unsigned long long>(settings.L);
-//         struct header h = {L, L, L};
-//         MPI_Status status;
-//         MPI_File_write(fh, &h, sizeof(h), MPI_BYTE, &status);
-//     }
-
-//     err =
-//         MPI_File_set_view(fh, headersize, MPI_DOUBLE, filetype, "native",
-//         info);
-//     CHECK_ERR(MPI_File_set_view)
-//     MPI_Barrier(comm);
-// }
 
 void Writer::write(int step, const GrayScott &sim)
 {
@@ -120,41 +105,21 @@ void Writer::write(int step, const GrayScott &sim)
     /* sim.u_noghost() provides a contiguous copy without the ghost cells */
     const std::vector<double> &u = sim.u_ghost();
     const std::vector<double> &v = sim.v_ghost();
-    _writer.BeginStep();
-    _writer.Put(varStep, step);
-    _writer.Put(varU, u.data());
-    _writer.EndStep();
+
+    MPI_Status status;
+    err = MPI_File_write_all(fh, u.data(), 1, memtype, &status);
+    CHECK_ERR(MPI_File_write_all)
 }
 
-// void Writer::write(int step, const GrayScott &sim)
-// {
-//     /* sim.u_ghost() provides access to the U variable as is */
-//     /* sim.u_noghost() provides a contiguous copy without the ghost cells */
-//     const std::vector<double> &u = sim.u_ghost();
-//     const std::vector<double> &v = sim.v_ghost();
-
-//     MPI_Status status;
-//     err = MPI_File_write_all(fh, u.data(), 1, memtype, &status);
-//     CHECK_ERR(MPI_File_write_all)
-// }
-
-void Writer::close() { _writer.Close(); }
-
-// void Writer::close()
-// {
-//     /* collectively close the file */
-//     err = MPI_File_close(&fh);
-//     CHECK_ERR(MPI_File_close);
-// }
+void Writer::close()
+{
+    /* collectively close the file */
+    err = MPI_File_close(&fh);
+    CHECK_ERR(MPI_File_close);
+}
 
 void Writer::print_settings()
 {
-    std::cout << "Simulation writes data using engine type: "
-              << _io.EngineType() << std::endl;
+    std::cout << "Simulation writes data using engine type:              "
+              << "native MPI-IO" << std::endl;
 }
-
-// void Writer::print_settings()
-// {
-//     std::cout << "Simulation writes data using engine type:              "
-//               << "native MPI-IO" << std::endl;
-// }
